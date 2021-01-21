f83f70d163fb15a4c3391d055866f53f833a4e0a
Yusypenko Oleksandr <o.yusipenko@gmail.com>
2021-01-21 15:53:07 +0200

    start working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+
+import "./App.css";
 
 function App() {
+  const [text, setText] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(5);
+  const [isTimeRunning, setIsTimeRunning] = useState(false);
+
+  function handleChange(e) {
+    const { value } = e.target;
+    setText(value);
+  }
+
+  function calculateWordCount(text) {
+    const wordsArr = text.trim().split(" ");
+    console.log(wordsArr);
+    return wordsArr.filter((word) => word !== "").length;
+  }
+
+  useEffect(() => {
+    if (isTimeRunning && timeRemaining > 0) {
+      setTimeout(() => {
+        setTimeRemaining((time) => time - 1);
+      }, 1000);
+    } else if (timeRemaining === 0) {
+      setIsTimeRunning(false);
+      console.log(isTimeRunning);
+    }
+  }, [timeRemaining, isTimeRunning]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Speed typing game</h1>
+      <textarea onChange={handleChange} value={text} />
+      <h4>Time reamining: {timeRemaining}</h4>
+      <button onClick={() => setIsTimeRunning(true)}>Start</button>
+      <h1>Word count: {calculateWordCount(text)}</h1>
     </div>
   );
 }
