48a75a10bc5fc8648210a55c65ef78beb0ac1d5e
Yusypenko Oleksandr <o.yusipenko@gmail.com>
2021-01-21 18:25:54 +0200

    add some functions and styles

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import React, { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const START_TIME = 7;
+
   const [text, setText] = useState("");
-  const [timeRemaining, setTimeRemaining] = useState(5);
+  const [timeRemaining, setTimeRemaining] = useState(START_TIME);
   const [isTimeRunning, setIsTimeRunning] = useState(false);
+  const [wordCount, setWordCount] = useState(0);
 
   function handleChange(e) {
     const { value } = e.target;
@@ -14,8 +17,18 @@ function App() {
 
   function calculateWordCount(text) {
     const wordsArr = text.trim().split(" ");
-    console.log(wordsArr);
     return wordsArr.filter((word) => word !== "").length;
+  }
+
+  function startClock() {
+    setIsTimeRunning(true);
+    setTimeRemaining(START_TIME);
+    setText("");
+  }
+
+  function endGame() {
+    setIsTimeRunning(false);
+    setWordCount(calculateWordCount(text));
   }
 
   useEffect(() => {
@@ -24,18 +37,26 @@ function App() {
         setTimeRemaining((time) => time - 1);
       }, 1000);
     } else if (timeRemaining === 0) {
-      setIsTimeRunning(false);
-      console.log(isTimeRunning);
+      endGame();
     }
+    // eslint-disable-next-line
   }, [timeRemaining, isTimeRunning]);
 
   return (
-    <div className="App">
-      <h1>Speed typing game</h1>
-      <textarea onChange={handleChange} value={text} />
-      <h4>Time reamining: {timeRemaining}</h4>
-      <button onClick={() => setIsTimeRunning(true)}>Start</button>
-      <h1>Word count: {calculateWordCount(text)}</h1>
+    <div className="wrapper">
+      <div className="app">
+        <h1>Speed typing game</h1>
+        <textarea
+          onChange={handleChange}
+          value={text}
+          disabled={!isTimeRunning}
+        />
+        <h4>Time reamining: {timeRemaining}</h4>
+        <button onClick={startClock} disabled={isTimeRunning}>
+          Start
+        </button>
+        <h1>Word count: {wordCount}</h1>
+      </div>
     </div>
   );
 }
